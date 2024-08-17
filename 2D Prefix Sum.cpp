40f00c2e4b31a2f8c34b230b2ea82beb63a594e6
Pritom Saha <ratulsaha61213@gmail.com>
2024-08-17 12:00:24 +0600

    Update 2D Prefix Sum.cpp

--- 2D Prefix Sum.cpp
+++ 2D Prefix Sum.cpp
@@ -1,3 +1,13 @@
+/*
+
+Topic: 2D Prefix Sum
+Source: https://usaco.guide/silver/more-prefix-sums?lang=cpp
+Complexity: build - O(n * m) get - O(1)
+Problem: https://cses.fi/problemset/task/1652
+
+*/
+
+
 const int N = 1e3 + 5;
 
 vector<vector<int>> matrix(N, vector<int>(N, 0));
@@ -14,3 +24,11 @@ int get(int rowMin, int colMin, int rowMax, int colMax) {
   return (matrix[rowMax][colMax] - matrix[rowMin - 1][colMax] - 
           matrix[rowMax][colMin - 1] + matrix[rowMin - 1][colMin - 1]);
 }
+
+
+/*
+
+Note: For 3D, 4D .. prefix sum 
+https://atcoder.jp/contests/abc366/editorial/10654
+
+*/
